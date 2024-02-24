6f1878e61331ceef7296346efb9d0dc5375e5ea4
metalhead <114188050+eckeriaue@users.noreply.github.com>
2024-02-24 21:41:13 +0300

    logger configure

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	config "github.com/eckeriaue/golang-url-shortener/internal"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug logger was enabled")
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
